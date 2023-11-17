9a5b91745753aff0947e9de6e0391e43a29ea47d
Anvi123123 <136162375+Anvi123123@users.noreply.github.com>
2023-11-18 00:47:04 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,17 @@
+
+// JavaScript function to redirect to a new webpage
+function redirectToOrderPage() {
+    // Change the URL to the desired destination
+    window.location.href = "Login-Register-Template/register.html";
+  }
+
+
+  function moveToFood(){
+    window.location.href="order food/order.html";
+  }
+
+
+//show an alert on clicking get updates
+  function showalert(){
+    alert("You have been subscribed to our weekly mail updates");
+}
